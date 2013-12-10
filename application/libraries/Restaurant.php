623e499f337502e5a813807ecfca403b72156544
Jonathan Irvin <djfoxyslpr@gmail.com>
2013-12-10 16:48:26 -0600

    working on the constructor and toString functions

--- application/libraries/Restaurant.php
+++ application/libraries/Restaurant.php
@@ -1,11 +1,34 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
 
 class Restaurant {
-
-    public function __contstruct()
-    {
 	
+	private $tags;
+	private $name;
+	private $description;	
+	
+    public function __contstruct($name,$tags,$description)
+    {
+		$name = $this->name;
+		$tags = $this->tags;
+		$description = $this->description;
     }
+	
+	public function toString()
+	{
+		print "<div id=\"portfolio\" class=\"super-list variable-sizes clearfix\">
+              <div class=\"element American Bakery tz_item\">
+                <div class=\"TzInner\">
+                  <div class=\"TzPortfolioMedia\"> <a href=\"http://placehold.it/400x300&text=$name\" class=\"prettyPhoto\" rel=\"prettyPhoto[id]\"> <img src=\"http://placehold.it/400x300&text=$name\"/>
+                    <div class=\"TzPortfolioDescription\">
+                      <h3 class=\"TzPortfolioTitle name\" itemprop=\"name\"> <em>$name</em> </h3>
+                      <span class=\"TzItemTag\">$description</span>
+                      <div class=\"r_plus \"></div>
+                    </div>
+                    </a> </div>
+                </div>
+                <!--Inner--> 
+              </div>";
+	}
 }
 
 /* End of file Restaurant.php */
