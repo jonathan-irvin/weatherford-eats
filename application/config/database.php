8392c39079ace8685d14fa717d9a0cf73e916b32
Jonathan Irvin <djfoxyslpr@gmail.com>
2014-01-16 14:42:47 -0600

    added db info

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'beacon42_wfdeats';
+$db['default']['password'] = '{?(kJtciRha#LgUko)';
+$db['default']['database'] = 'beacon42_wfordeats';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
