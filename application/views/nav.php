801ef206532dde486c7811bfd2005becf9b03837
Jonathan Irvin <djfoxyslpr@gmail.com>
2014-01-25 13:13:16 -0600

    added nav

---
+++ application/views/nav.php
@@ -0,0 +1,13 @@
+<!-- THE FILTER STYLED FOR MEGAFOLIO -->
+
+<div class="filter_padder" >
+  <div class="filter_wrapper" style="max-width:650px;">
+    <div class="filter selected" data-category="cat-all">ALL</div>
+    <div class="filter" data-category="cat-one">INTERIOR</div>
+    <div class="filter" data-category="cat-two">NATURE</div>
+    <div class="filter" data-category="cat-three">FOOD</div>
+    <div class="filter last-child" data-category="cat-four">MODELS</div>
+    <div class="clear"></div>
+  </div>
+</div>
+<div class="clear"></div>
